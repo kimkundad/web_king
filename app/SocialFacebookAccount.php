20a4c7d7a9fadc07779d3d28fa34dea04980864c
kimkundad <kim.kundad@gmail.com>
2017-12-20 19:13:36 +0700

    golf3

--- app/SocialFacebookAccount.php
+++ app/SocialFacebookAccount.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class SocialFacebookAccount extends Model
 {
+  protected $table = 'facebook_login';
+
   protected $fillable = ['user_id', 'provider_user_id', 'provider'];
 
   public function user()
